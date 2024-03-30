da1a7af49bc8621b240934cb2153e1e6e57bd002
Dima <you@example.com>
2024-03-30 16:04:13 +0200

    update

--- js/languageSwitcher.js
+++ js/languageSwitcher.js
@@ -2,11 +2,19 @@ function changeLanguage(select) {
     var selectedOption = select.value;
     var baseUrl = window.location.origin;
     var path = window.location.pathname;
-    
-    // Визначаємо шлях до потрібної мовної версії сторінки
-    var newPath = path.replace(/_(en|ru|th)\.html$/, '') + '_' + selectedOption + '.html';
-    
+  
+    // Визначаємо базову частину шляху (без суфікса мови і розширення .html)
+    var basePath = path.replace(/_(en|ru|th)\.html$/, '').replace(/\.html$/, '');
+  
+    // Враховуємо, що для основної мови ми використовуємо файл index.html без суфікса
+    var newPath;
+    if(selectedOption === "en") { // Припустимо, що англійська мова є основною і використовує index.html
+      newPath = basePath + '.html'; // для англійської версії
+    } else {
+      newPath = basePath + '_' + selectedOption + '.html'; // для інших мов
+    }
+  
     // Перенаправляємо користувача на вибрану мовну версію
-    console.log("click")
     window.location.href = baseUrl + newPath;
   }
+  
